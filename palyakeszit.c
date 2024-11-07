ae28e2dc4033d238afbfcac4281c3f1152f7bf8c
Krister01 <thewhiteeye27@gmail.com>
2024-11-07 13:58:24 +0100

    Update palyakeszit.c
    MUKIK (egy része..)

--- palyakeszit.c
+++ palyakeszit.c
@@ -10,16 +10,23 @@ bool palya_lefog(Palya *p, int szelesseg, int magassag, int aknadb){
     p->adat = (int **) malloc(magassag * sizeof(int*));
     for (int y = 0; y < magassag; y++)
         p->adat[y] = (int *) malloc(szelesseg * sizeof(int));
-    if (p->adat == NULL || p->adat[0] == NULL)
+    if (p->adat == NULL)
         return false;
     return true;
-} // Nem működik függvényként csak mainen belül
+} // Nem mûködik függvényként csak mainen belül
 
 void palya_felszab(Palya *p){
     for (int y = 0; y < p->magassag; ++y)
         free(p->adat[y]);
     free(p->adat);
-} //Nem működik függvényként csak mainen belül
+} //Nem mûködik függvényként csak mainen belül
+
+bool ures_feltolt(Palya *p){
+    for(int y = 0; y < p->magassag; y++)
+        for(int x = 0; x < p->szelesseg; x++)
+            p->adat[y][x] = 0;
+    return true;
+}//Kész
 
 bool bennevan(int keresett, int *t, int meret){
     for (int i = 0; i < meret; i++)
@@ -29,14 +36,22 @@ bool bennevan(int keresett, int *t, int meret){
 }//Kész
 
 bool akna_feltolt(Palya *p){
-    int *ak = (int*) malloc(p.aknadb * sizeof(int));
+    int *ak = (int*) malloc(p->aknadb* sizeof(int));
+    if (ak == NULL) {return false;}
     int aknahely;
-    for (int i = 0; i < p.aknadb; i++){
-        aknahely = rand() % p.magassag * p.szelesseg;
-        if (!bennevan(aknahely,ak,i+1))
+    for (int i = 0; i < p->aknadb; i++){
+        aknahely = rand() % (p->magassag * p->szelesseg);
+        if (!bennevan(aknahely,ak,i))
             ak[i] = aknahely;
         else
             i--;
     }
-    for (int i; i < p.aknadb;
-}
+    int x, y;
+    for (int i = 0; i < p->aknadb;i++){
+        x = ak[i] % p->szelesseg;
+        y = ak[i] / p->szelesseg+1;
+        p->adat[y][x] = 1;
+    }
+    free(ak);
+    return true;
+}//Kész
