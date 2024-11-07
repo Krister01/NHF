72c6f480197296074d6c4765595339544bbcbe2e
Krister01 <thewhiteeye27@gmail.com>
2024-11-07 11:48:40 +0100

    Update palyakeszit.c
    Akna feltöltés in progress

--- palyakeszit.c
+++ palyakeszit.c
@@ -13,4 +13,30 @@ bool palya_lefog(Palya *p, int szelesseg, int magassag, int aknadb){
     if (p->adat == NULL || p->adat[0] == NULL)
         return false;
     return true;
+} // Nem működik függvényként csak mainen belül
+
+void palya_felszab(Palya *p){
+    for (int y = 0; y < p->magassag; ++y)
+        free(p->adat[y]);
+    free(p->adat);
+} //Nem működik függvényként csak mainen belül
+
+bool bennevan(int keresett, int *t, int meret){
+    for (int i = 0; i < meret; i++)
+        if (keresett == t[i])
+            return true;
+    return false;
+}//Kész
+
+bool akna_feltolt(Palya *p){
+    int *ak = (int*) malloc(p.aknadb * sizeof(int));
+    int aknahely;
+    for (int i = 0; i < p.aknadb; i++){
+        aknahely = rand() % p.magassag * p.szelesseg;
+        if (!bennevan(aknahely,ak,i+1))
+            ak[i] = aknahely;
+        else
+            i--;
+    }
+    for (int i; i < p.aknadb;
 }
